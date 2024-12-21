2ff4d61533b3cf249e4a0fe143c2a5cdb96aa02a
Jabba Laci <jabba.laci@gmail.com>
2024-12-21 23:02:03 +0100

    play sound when process terminates

---
+++ 20241221-play-sound-when-process-terminates/example1.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "working..."
+sleep 1
---
+++ 20241221-play-sound-when-process-terminates/example2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print("CPU intensive task...")
+
+    cnt = 0
+    while True:
+        cnt += 1
+
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
